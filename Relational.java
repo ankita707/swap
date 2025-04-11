e63dd34208dc3d1353fa65fc7c180aac792425b2
veeres-javali18 <sharatjoshi1@gmail.com>
2025-04-11 10:45:12 +0530

    Save

---
+++ Relational.java
@@ -0,0 +1,14 @@
+public class Relational {
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=20;
+        System.out.println("A>B="+(a>b));
+        System.out.println("A<B="+(a<b));
+        System.out.println("A>=B="+(a>=b));
+        System.out.println("A<=B="+(a<=b));
+        System.out.println("A==B="+(a==b));
+        System.out.println("A!=B="+(a!=b));
+    }
+    
+}
