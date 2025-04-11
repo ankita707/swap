d43707440d53b028966cc959614e1595ec787a63
veeres-javali18 <sharatjoshi1@gmail.com>
2025-04-11 10:35:36 +0530

    save

---
+++ datatype.java
@@ -0,0 +1,20 @@
+public class datatype {
+    public static void main(String[] args)
+    {
+       float f=9.3f;
+       char a='A';
+       int b=20;
+       double c=3.44;
+       boolean d=true;
+       byte e=0;
+       long g=787876;
+       System.out.println(f);
+       System.out.println(a);
+       System.out.println(b);
+       System.out.println(c);
+       System.out.println(d);
+       System.out.println(e);
+       System.out.println(g);
+    }
+    
+}
